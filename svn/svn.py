1a137869d9dad7c50de1254c64d87cb68e50bd91
Honey <tinygg@163.com>
2017-07-21 18:21:57 +0800

    bk to remote dir

--- svn/svn.py
+++ svn/svn.py
@@ -4,6 +4,7 @@ import datetime
 
 config = file('dump.txt','r')
 d = config.readline().strip()
+to_dir = config.readline().strip()
 projects = config.readlines()
 
 os.chdir(d)
@@ -15,7 +16,8 @@ for item in projects:
 	if item.count > 0:
 		now = datetime.datetime.now()
 		now_str = '%d.%d.%d.%d.%d' %(now.year,now.month,now.day,now.hour,now.minute)
-		p = subprocess.Popen("svnadmin dump %s > %s.src.%s.dmp" % (item,item,now_str), stdout=subprocess.PIPE, shell=True)
+		os.mkdir(('%s\%s' % (to_dir, now_str)))
+		p = subprocess.Popen("svnadmin dump %s > %s\%s\%s.src.%s.dmp" % (item,to_dir,now_str,item,now_str), stdout=subprocess.PIPE, shell=True)
 		(output, err) = p.communicate()
 		print('%s is dump over' % item)
 	else:
