b97f73943b34590e0397618a466a6f8aefd32c9b
Honey <tinygg@163.com>
2017-07-24 16:09:38 +0800

    支持增量备份,fix bug 重复创建目录导致只能导出第一个项个项目的问题

--- svn/svn.py
+++ svn/svn.py
@@ -19,11 +19,21 @@ now_str = '%d.%d.%d.%d.%d' %(now.year,now.month,now.day,now.hour,now.minute)
 for item in projects:
 	item = item.strip()
 	if item.count > 0:
-		os.mkdir(('%s\%s' % (to_dir, now_str)))
-		p = subprocess.Popen("svnadmin dump %s > %s\%s\%s.src.%s.dmp" % (item,to_dir,now_str,item,now_str), stdout=subprocess.PIPE, shell=True)
+		d_dir = '%s\%s' % (to_dir, now_str);
+		if not os.path.exists(d_dir):
+			os.mkdir(d_dir)
+			
+		if '@' in item:
+			proj = item.split('@')[0]
+			from_version = item.split('@')[1]
+			p = subprocess.Popen("svnadmin dump %s --revision %s --incremental > %s\%s\%s.src.%s.dmp" % (proj, from_version, to_dir, now_str, proj, now_str), stdout=subprocess.PIPE, shell=True)
+		else:	
+			p = subprocess.Popen("svnadmin dump %s > %s\%s\%s.src.%s.dmp" % (item,to_dir,now_str,item,now_str), stdout=subprocess.PIPE, shell=True)
 		(output, err) = p.communicate()
 		print('%s is dump over' % item)
 	else:
-		print('path error:%s' % item)
+		print('path error:%s' % item)	
 	
 print('every thing dump over')
+
+
